3a9430a036e68238af6b7fc063687cc8f7932af6
luizSmith <luiz.victor100@hotmail.com>
2024-12-05 02:41:27 -0300

    refactor: resolvendo conflito

--- database.sql
+++ database.sql
@@ -50,3 +50,41 @@ CREATE TABLE fazenda_cultura (
     FOREIGN KEY (id_cultura) REFERENCES cultura(id) ON DELETE CASCADE,
     FOREIGN KEY (id_fazenda) REFERENCES fazenda(id) ON DELETE CASCADE
 );
+
+INSERT INTO cultura (nome)
+VALUES
+  ('Soja'),
+  ('Milho'),
+  ('Algodão'),
+  ('Café'),
+  ('Cana de Açúcar');
+  
+ 
+ INSERT INTO estado (uf, nome) VALUES
+('AC', 'Acre'),
+('AL', 'Alagoas'),
+('AP', 'Amapá'),
+('AM', 'Amazonas'),
+('BA', 'Bahia'),
+('CE', 'Ceará'),
+('DF', 'Distrito Federal'),
+('ES', 'Espírito Santo'),
+('GO', 'Goiás'),
+('MA', 'Maranhão'),
+('MT', 'Mato Grosso'),
+('MS', 'Mato Grosso do Sul'),
+('MG', 'Minas Gerais'),
+('PA', 'Pará'),
+('PB', 'Paraíba'),
+('PR', 'Paraná'),
+('PE', 'Pernambuco'),
+('PI', 'Piauí'),
+('RJ', 'Rio de Janeiro'),
+('RN', 'Rio Grande do Norte'),
+('RS', 'Rio Grande do Sul'),
+('RO', 'Rondônia'),
+('RR', 'Roraima'),
+('SC', 'Santa Catarina'),
+('SP', 'São Paulo'),
+('SE', 'Sergipe'),
+('TO', 'Tocantins');
